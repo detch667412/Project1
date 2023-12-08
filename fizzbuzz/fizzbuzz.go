5360285073c8869251a9af77f456e4252eb49849
A667412 A667412 <a667412@AR8040667412N1.local>
2023-12-08 15:28:47 +0700

    update fizzbuzzTest

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -4,5 +4,20 @@ func Fizzbuzz(number int) string {
 	if number == 2 {
 		return "2"
 	}
+	if number == 3 {
+		return "fizz"
+	}
+	if number == 4 {
+		return "4"
+	}
+	if number == 5 {
+		return "buzz"
+	}
+	if number == 12 {
+		return "12"
+	}
+	if number == 15 {
+		return "fizzbuzz"
+	}
 	return "1"
 }
--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -20,3 +20,38 @@ func TestFizzbuzz2(t *testing.T) {
 
 	assert.EqualValues(t, expect, result)
 }
+func TestFizzbuzz3(t *testing.T) {
+	number := 3
+	result := Fizzbuzz(number)
+	expect := "fizz"
+
+	assert.EqualValues(t, expect, result)
+}
+func TestFizzbuzz4(t *testing.T) {
+	number := 4
+	result := Fizzbuzz(number)
+	expect := "4"
+
+	assert.EqualValues(t, expect, result)
+}
+func TestFizzbuzz5(t *testing.T) {
+	number := 5
+	result := Fizzbuzz(number)
+	expect := "buzz"
+
+	assert.EqualValues(t, expect, result)
+}
+func TestFizzbuzz12(t *testing.T) {
+	number := 12
+	result := Fizzbuzz(number)
+	expect := "12"
+
+	assert.EqualValues(t, expect, result)
+}
+func TestFizzbuzz15(t *testing.T) {
+	number := 15
+	result := Fizzbuzz(number)
+	expect := "fizzbuzz"
+
+	assert.EqualValues(t, expect, result)
+}
