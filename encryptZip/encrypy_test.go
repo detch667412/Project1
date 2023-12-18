59de2bb4b443adc2a9f5e703383d8ca091fd0a5d
detch667412 <detch.d@arise.tech>
2023-12-18 14:19:36 +0700

    add encryptZip Function

---
+++ encryptZip/encrypy_test.go
@@ -0,0 +1,46 @@
+package encryptzip
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncryptZip(t *testing.T) {
+	t.Run("Encrypt and zip an existing file", func(t *testing.T) {
+		// Create a temporary test file
+		testFileName := "testfile.txt"
+		err := createTestFile(testFileName)
+		assert.NoError(t, err, "Error creating test file")
+		defer deleteTestFile(testFileName)
+		defer os.Remove("output.zip")
+
+		// Test the function
+		err = EncryptZip(testFileName)
+		assert.NoError(t, err, "Error encrypting and zipping the file")
+	})
+
+	t.Run("Fail to open non-existing file", func(t *testing.T) {
+		// Test case 2: File not found
+		err := EncryptZip("nonexistentfile.txt")
+		assert.Error(t, err, "Expected error for nonexistent file")
+	})
+}
+
+// Helper function to create a test file with some content
+func createTestFile(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("Test content for encryption")
+	return err
+}
+
+// Helper function to delete the test file
+func deleteTestFile(filename string) {
+	_ = os.Remove(filename)
+}
--- main.go
+++ main.go
@@ -1,52 +1,6 @@
 package main
 
-import (
-	"io"
-	"log"
-	"os"
-
-	"github.com/yeka/zip"
-)
-
 func main() {
-	err := EncryptZip("input.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	//Dont forget to write unit tesr for "EncryptZip"
-}
-
-func EncryptZip(fileToZip string) error {
-	// Open the file to be zipped
-	file, err := os.Open(fileToZip)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// Create a new ZIP file
-	zipFile, err := os.Create("output.zip")
-	if err != nil {
-		return err
-	}
-	defer zipFile.Close()
-
-	// Create a ZIP writer
-	zipWriter := zip.NewWriter(zipFile)
-	defer zipWriter.Close()
-
-	w, err := zipWriter.Encrypt(fileToZip, `golang`, zip.AES256Encryption)
-	if err != nil {
-		return err
-	}
-
-	// Copy the content of the existing file to the ZIP archive
-	_, err = io.Copy(w, file)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("File '%s' successfully zipped to 'output.zip'\n", fileToZip)
-	return nil
 }
